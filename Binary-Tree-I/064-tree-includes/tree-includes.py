d93dce0bdb50b5024ea52c3023eb3b022a3f502e
Somit Meshram <somitmeshram3256@gmail.com>
2026-08-08 13:14:51 +0530

    Solved 'tree includes' (python3)

--- Binary-Tree-I/064-tree-includes/tree-includes.py
+++ Binary-Tree-I/064-tree-includes/tree-includes.py
@@ -6,17 +6,28 @@
 
 def tree_includes(root, target):
   #DFS
-  def dfs(root,target):
-    if not root:
-      return False
-    stack = [root]
-    while stack:
-      current = stack.pop()
-      if current.val == target:
+  # def dfs(root,target):
+  #   if not root:
+  #     return False
+  #   stack = [root]
+  #   while stack:
+  #     current = stack.pop()
+  #     if current.val == target:
+  #       return True
+  #     if current.left:
+  #       stack.append(current.left)
+  #     if current.right:
+  #       stack.append(current.right)
+  #   return False
+  # return dfs(root,target)
+
+
+    def DFS_recusrive(root,target):
+      if not root:
+        return False
+      if root.val == target:
         return True
-      if current.left:
-        stack.push(current.left)
-      if current.right:
-        stack.push(current.right)
-    return False
-  return dfs(root,target)
+      return DFS_recusrive(root.left,target) or DFS_recusrive(root.right,target)
+
+    return DFS_recusrive(root,target)
+      
