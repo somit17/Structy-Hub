1e9a517607fb9cb7998e97cf86fad5983bc20ca3
Somit Meshram <somitmeshram3256@gmail.com>
2026-08-08 13:18:00 +0530

    Solved 'tree sum' (python3)

---
+++ Binary-Tree-I/063-tree-sum/tree-sum.py
@@ -0,0 +1,20 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def tree_sum(root):
+  def DFS(root):
+    if not root:
+      return 0
+    stack = [root]
+    total = 0
+    while stack:
+      current = stack.pop()
+      total+= current.val
+      if current.left:
+        stack.append(current.left)
+      if current.right:
+        stack.append(current.right)
+  pass # todo
