ebfa702981dab0781b77bf3e6916e25a5981cdcf
Somit Meshram <somitmeshram3256@gmail.com>
2026-08-08 13:05:37 +0530

    Solved 'breadth first values' (python3)

---
+++ Binary-Tree-I/061-breadth-first-values/breadth-first-values.py
@@ -0,0 +1,8 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+from collections import deque
+def breadth_first_values(root):
+  pass # todo
