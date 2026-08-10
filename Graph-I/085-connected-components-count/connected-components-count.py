d6d6f2af32cb09759a0f4e30905dded640a17171
Somit Meshram <somitmeshram3256@gmail.com>
2026-08-10 22:08:58 +0530

    Solved 'connected components count' (python3)

---
+++ Graph-I/085-connected-components-count/connected-components-count.py
@@ -0,0 +1,18 @@
+def connected_components_count(graph):
+  count = 0
+  visited = set()
+  for node in graph:
+    explore(graph,node,visited):
+    count+=1
+
+  return count
+    
+
+def explore(graph,node,visited):
+  if node in visited:
+    return False
+
+  visited.add(node)
+
+  for neighbor in graph[node]:
+    explore(graph,neighbor,visited)
